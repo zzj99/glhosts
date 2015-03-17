c2e0939d665b5da12cae7c280f1801111884139f
zzj99 <zzj.99@hotmail.com>
2015-03-17 10:06:56 +0800

    use OrderedDict to insure the invariance of the sequence of hosts.

--- g0og1e/hosts.py
+++ g0og1e/hosts.py
@@ -1,8 +1,8 @@
 #!usr/bin/env python
 # check and update google hosts from specific URL
 # written in python version 2.7.6
-# works with python version in the range [2.6, 3)
-# by zzj.99@hotmail.com, 
+# works for python version 2.7+, but not python 3
+# by zzj.99@hotmail.com
 # last updated in 2015/03
 
 import urllib2      # for opening url, python2 grammar
@@ -12,8 +12,9 @@ import re           # for regular expression
 import datetime     # for recording time of updated hosts
 import tempfile     # for temporary file
 import ctypes       # for windows
+import collections  # for OrderedDict
 
-# check if user is admin
+# check if user is admin, required for changing the hosts file
 try:
     is_admin = (os.getuid() == 0)
 except AttributeError:
@@ -32,51 +33,71 @@ if (os.name == 'nt'): # Windows
 else: # Linux or MacOS
     hosts = '/etc/hosts'
 
-#---------------------------------------------------------------------------
-# function for updating hosts
+""" function that creates an ordered dictionary from hosts file """
+def create_hosts_dict(hosts_file):
+    hosts_dict = collections.OrderedDict() # ordered dictionary
+    f = open(hosts_file,"r")    
+    for line in f:
+        pattern = re.match(r"^\s*(\d+\.\d+\.\d+\.\d+)\s+(.*)\s*",line)
+        if (pattern):
+            hosts_dict[pattern.group(2)] = pattern.group(1)
+
+    return hosts_dict
+
+""" function for updating hosts """
 def update_hosts():
-    # http://www.360kb.com/kb/2_143.html is for google android 
-    # another URL is http://freedom.txthinking.com/hosts
-    hosts_url = "http://www.360kb.com/kb/2_122.html"
+    # Websites that contain hosts, in case the selected one is unavailable
+    # http://freedom.txthinking.com/hosts
+    # http://www.360kb.com/kb/2_122.html
+    hosts_url = "http://freedom.txthinking.com/hosts"
 
     # try to reach the URL for updating hosts
     try:
         response = urllib2.urlopen(hosts_url, timeout=5)
         now = datetime.datetime.now()
-        fname = tempfile.mktemp()
-        google_hosts = open(fname, "w")
+        temp_hosts = tempfile.mktemp()
+        f_hosts = open(temp_hosts, "w")
         for i, line in enumerate(response):            
             line = re.sub(r"<[^>]*>", "", line)  # remove html tags
             line = re.sub(r"&nbsp;", " ", line)  # replace &nbsp; to white space
             ip = re.match(r"^\s*\d+\.\d+\.\d+\.\d+", line) # ip
             comment = re.match(r"^\s*#",line) # comments
             if (ip or comment):
-                google_hosts.write(line)
+                f_hosts.write(line)
 
-        google_hosts.close()
+        f_hosts.close()
         
         # make a backup for hosts
         date = now.strftime("%Y%m%d")
         hosts_bak = "hosts."+ date
         shutil.copyfile(hosts, hosts_bak)
-        print " old hosts is copied to current directory as ",hosts_bak
+        print "old hosts is copied to current directory as ", hosts_bak
         
-        # copy content to hosts
-    # @todo merge with old?
-        google_hosts = open(fname,"r")
-        content = google_hosts.read()
+        # update hosts dictionary ( key is name, value is ip )
+        hosts_dict = create_hosts_dict(hosts)
+        new_dict = create_hosts_dict(temp_hosts)
+        hosts_dict.update(new_dict)
+
+        # open file for writing hosts 
         hosts_file = open(hosts, "w")
-        hosts_file.write(content)
-        google_hosts.close()
+
+        # write update information
+        info = "# based on " + hosts_url + "\n# last update: " + date + "\n"
+        hosts_file.write(info) 
+        
+        # write hosts from hosts dictionary
+        for name, ip in hosts_dict.items():
+            line = ip + "  " + name + "\n"
+            hosts_file.write(line)
         hosts_file.close()
-        print " hosts updated! see ",hosts
+        print "hosts updated! see ",hosts
 
     except urllib2.URLError, e:
         print ("Can't reach the URL for updating hosts!")
 
     return
-#---------------------------------------------------------------------------
-# function for checking if google ip exists and works, if not, update hosts
+
+""" check if google ip exists and works, if not, update hosts """
 def check_and_update_hosts():
     print "checking current hosts ..."
     # try to get the ip address for "www.google.com" in current hosts file
@@ -107,6 +128,6 @@ def check_and_update_hosts():
         update_hosts()
 
     return
-#---------------------------------------------------------------------------
+
 # run script to check and update hosts
-check_and_update_hosts(
+check_and_update_hosts()
